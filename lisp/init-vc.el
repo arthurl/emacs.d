e7a75fe7ec9826925b8a8ce2d1c6090c91b9c78e
Arthur Lee <me@arthur.li>
2016-04-23 15:40:09 +0800

    Improve editing

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -1,5 +1,6 @@
 (when (maybe-require-package 'diff-hl)
   (add-hook 'prog-mode-hook 'diff-hl-mode)
+  (add-hook 'text-mode-hook 'diff-hl-mode)
   (add-hook 'vc-dir-mode-hook 'diff-hl-mode)
   (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
 
