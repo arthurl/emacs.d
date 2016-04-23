30a7c0eea72a5ca4dcbc22a685a77298ef7c7e6c
Arthur Lee <me@arthur.li>
2016-04-23 17:11:03 +0800

    Improve haskell-mode

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -50,5 +50,38 @@
   (define-key interactive-haskell-mode-map (kbd "M-N") 'haskell-goto-next-error)
   (define-key interactive-haskell-mode-map (kbd "M-P") 'haskell-goto-prev-error))
 
+;; Don't popup errors
+(setq-default haskell-interactive-popup-errors nil)
+
+
+
+;; Add quotes to compile path to handle folders names with spaces
+(setq-default
+ haskell-compile-cabal-build-command
+ "cd \"%s\" && cabal build --ghc-option=-ferror-spans"
+ haskell-compile-cabal-build-alt-command
+ "cd \"%s\" && cabal clean -s && cabal build --ghc-option=-ferror-spans"
+ haskell-compile-command
+ "ghc -Wall -ferror-spans -fforce-recomp -c \"%s\"")
+
+(after-load 'haskell-mode
+  (define-key haskell-mode-map (kbd "<f5>") 'haskell-compile))
+
+
+
+;; Speedbar
+(add-hook 'haskell-mode-hook 'haskell-decl-scan-mode)
+(after-load 'speedbar
+  (speedbar-add-supported-extension ".hs")
+  (speedbar-add-supported-extension ".lhs")
+  (speedbar-add-supported-extension ".hsc"))
+
+;; Set up tagging if hasktags exists
+(if (executable-find "hasktags")
+    (setq-default haskell-tags-on-save t)
+  (after-load 'haskell-mode
+    ;; If hasktags does not exist, don't bother to try to use tags.
+    (define-key haskell-mode-map (kbd "M-.") 'haskell-mode-jump-to-def)))
+
 
 (provide 'init-haskell)
