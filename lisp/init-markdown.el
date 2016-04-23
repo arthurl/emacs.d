5103e48a7d091fe9809c2b47ce14f38193c13624
Arthur Lee <me@arthur.li>
2016-04-23 17:09:01 +0800

    Improve markdown-mode

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -1,7 +1,16 @@
 (when (maybe-require-package 'markdown-mode)
   (add-auto-mode 'markdown-mode "\\.md\\.html\\'")
+
+  (after-load 'markdown-mode
+    (define-key markdown-mode-map (kbd "C-c C-x C-b") #'org-toggle-checkbox))
+  (after-load 'gfm-mode
+    (define-key gfm-mode-map (kbd "C-c C-x C-b") #'org-toggle-checkbox))
+
   (after-load 'whitespace-cleanup-mode
     (push 'markdown-mode whitespace-cleanup-mode-ignore-modes)))
+
+;; Enable syntax highlighting for math LaTeX fragments
+(setq-default markdown-enable-math t)
 
 
 (provide 'init-markdown)
