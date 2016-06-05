63ae913ee945e419e2b7168666b0215f686cd2eb
Arthur Lee <me@arthur.li>
2016-06-05 17:34:48 +0800

    Improve projectile-mode

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -12,7 +12,17 @@
      '(:eval
        (if (file-remote-p default-directory)
            " Pr"
-         (format " Pr[%s]" (projectile-project-name)))))))
+         (format " Pr[%s]" (projectile-project-name))))))
+
+  ;; More reasonable time to expire remote cache
+  (setq-default projectile-file-exists-remote-cache-expire (* 3 60))
+  ;; Open project dir when switching to project
+  (setq-default projectile-switch-project-action #'projectile-dired)
+
+  ;; projectile-project-p is not autoloaded in the library :/
+  (autoload #'projectile-project-p "projectile")
+  ;; projectile-project-root is not autoloaded in the library
+  (autoload #'projectile-project-root "projectile"))
 
 
 (provide 'init-projectile)
