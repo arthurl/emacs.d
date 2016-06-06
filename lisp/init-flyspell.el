4a855cb5c2015d7be052937408b37ce13de24002
Arthur Lee <me@arthur.li>
2016-06-06 20:35:59 +0800

    Enable spell-check for text-mode

--- lisp/init-flyspell.el
+++ lisp/init-flyspell.el
@@ -6,5 +6,12 @@
 (after-load 'flyspell
   (add-to-list 'flyspell-prog-text-faces 'nxml-text-face))
 
+(add-hook 'text-mode-hook 'flyspell-mode)
+
+(eval-after-load "flyspell"
+  '(progn
+     (define-key flyspell-mouse-map [down-mouse-3] #'flyspell-correct-word)
+     (define-key flyspell-mouse-map [mouse-3] nil)))
+
 
 (provide 'init-flyspell)
