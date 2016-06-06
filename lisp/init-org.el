0f4b19c74057f57e3afb22ca602edb1a20a82231
Arthur Lee <me@arthur.li>
2016-06-06 20:31:29 +0800

    Improve org-mode

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -367,7 +367,7 @@ typical word processor."
      (dot . t)
      (emacs-lisp . t)
      (gnuplot . t)
-     (haskell . nil)
+     (haskell . t)
      (latex . t)
      (ledger . t)
      (ocaml . nil)
@@ -379,6 +379,20 @@ typical word processor."
      (,(if (locate-library "ob-sh") 'sh 'shell) . t)
      (sql . t)
      (sqlite . t))))
+
+
+
+;;; Formatting in org-mode
+
+;; Syntax highlighting in code bocks in org-mode
+(setq-default org-src-fontify-natively t)
+
+;; Preserve whitespace when tangling
+;; see http://orgmode.org/manual/Editing-source-code.html
+(setq-default org-src-preserve-indentation t)
+
+;; Use different bullet for sub-lists
+(setq-default org-list-demote-modify-bullet '(("+" . "-") ("-" . "+") ("*" . "+")))
 
 
 (provide 'init-org)
