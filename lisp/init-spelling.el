8fd08d8b4ca4eaa2c23ab752a4684f0bb176a8c6
Arthur Lee <me@arthur.li>
2016-06-06 20:35:59 +0800

    Enable spell-check for text-mode

--- lisp/init-spelling.el
+++ lisp/init-spelling.el
@@ -11,5 +11,12 @@
   (with-eval-after-load 'flyspell
     (add-to-list 'flyspell-prog-text-faces 'nxml-text-face)))
 
+(add-hook 'text-mode-hook 'flyspell-mode)
+
+(eval-after-load "flyspell"
+  '(progn
+     (define-key flyspell-mouse-map [down-mouse-3] #'flyspell-correct-word)
+     (define-key flyspell-mouse-map [mouse-3] nil)))
+
 (provide 'init-spelling)
 ;;; init-spelling.el ends here
