56f36472a3200e0d89eb475150822db0ed913055
Arthur Lee <me@arthur.li>
2017-09-28 19:13:28 +0800

    Revert "Don't use C-<arrows> for windmove on Windows"
    This reverts commit 345d2a8583b06254d3c0f8f265b72fd4779d3f14.

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -88,8 +88,7 @@ Call a second time to restore the original window configuration."
 
 
 
-(unless (memq window-system '(nt w32))
-  (windmove-default-keybindings 'control))
+(windmove-default-keybindings 'control)
 
 
 (provide 'init-windows)
