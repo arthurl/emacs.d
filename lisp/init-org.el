cfde0b6f33a41a6a6934af017befd9254371d6a9
Arthur Lee <me@arthur.li>
2021-01-03 12:44:16 +0800

    Generalise some of prose-mode to writeroom-mode

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -62,7 +62,11 @@
 
 
 
-(maybe-require-package 'writeroom-mode)
+(when (maybe-require-package 'writeroom-mode)
+  (with-eval-after-load 'writeroom-mode
+    (setq-default writeroom-extra-line-spacing 0.2)
+    (setq-default writeroom-global-effects (delete 'writeroom-set-fullscreen writeroom-global-effects))
+    (setq-default writeroom-local-effects '(visual-line-mode buffer-face-mode))))
 
 (define-minor-mode prose-mode
   "Set up a buffer for prose editing.
@@ -72,32 +76,22 @@ typical word processor."
   :init-value nil :lighter " Prose" :keymap nil
   (if prose-mode
       (progn
-        (when (fboundp 'writeroom-mode)
-          (writeroom-mode 1))
-        (setq truncate-lines nil)
-        (setq word-wrap t)
-        (setq cursor-type 'bar)
         (when (eq major-mode 'org)
           (kill-local-variable 'buffer-face-mode-face))
-        (buffer-face-mode 1)
+        (when (fboundp 'writeroom-mode)
+          (writeroom-mode 1))
+        (setq cursor-type 'bar)
         ;;(delete-selection-mode 1)
         (setq-local blink-cursor-interval 0.6)
         (setq-local show-trailing-whitespace nil)
-        (setq-local line-spacing 0.2)
         (setq-local electric-pair-mode nil)
-        (ignore-errors (flyspell-mode 1))
-        (visual-line-mode 1))
-    (kill-local-variable 'truncate-lines)
-    (kill-local-variable 'word-wrap)
+        (ignore-errors (flyspell-mode 1)))
     (kill-local-variable 'cursor-type)
     (kill-local-variable 'blink-cursor-interval)
     (kill-local-variable 'show-trailing-whitespace)
-    (kill-local-variable 'line-spacing)
     (kill-local-variable 'electric-pair-mode)
-    (buffer-face-mode -1)
     ;; (delete-selection-mode -1)
     (flyspell-mode -1)
-    (visual-line-mode -1)
     (when (fboundp 'writeroom-mode)
       (writeroom-mode 0))))
 
