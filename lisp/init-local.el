5c592ea97d492edf06170e19b06de4b65a84b177
Arthur Lee <me@arthur.li>
2022-09-08 10:01:39 +0100

    {Personal} eglot: Don't confirm server edits

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -3,6 +3,10 @@
 ;; Markdown
 (setq markdown-command "pandoc -f markdown+tex_math_single_backslash+smart -t html -s --mathjax --highlight-style=pygments")
 
+;; Eglot
+(with-eval-after-load 'eglot
+  (setq eglot-confirm-server-initiated-edits nil))
+
 
 
 ;;; Global customisations
