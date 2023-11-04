56188975d54e8dd5c237d8cf0591cf224cd3b4da
Arthur Lee <git@arthur.li>
2023-11-04 11:13:34 +0000

    haskell: Use eglot

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -30,6 +30,19 @@
       :lighter " Orm"))
 
   (with-eval-after-load 'haskell-mode
+    (with-eval-after-load 'flymake
+      ;; Use compilation-mode's keybindings for flymake
+      (define-key haskell-mode-map (kbd "M-g M-n") 'flymake-goto-next-error)
+      (define-key haskell-mode-map (kbd "M-g M-p") 'flymake-goto-prev-error))
+
+    (when (maybe-require-package 'eglot)
+      ;; if we're using LSP, we don't need other flymake checkers
+      ;;(remove-hook 'flymake-diagnostic-functions 'haskell-flymake)
+      (add-hook 'haskell-mode-hook #'eglot-ensure)
+      (add-hook 'eglot-managed-mode-hook (lambda () (setq-local eldoc-documentation-function #'eldoc-documentation-default)))
+      (with-eval-after-load 'eglot
+        (define-key haskell-mode-map (kbd "C-'") 'eglot-code-actions)))
+
     (define-key haskell-mode-map (kbd "C-c h") 'hoogle)
     (define-key haskell-mode-map (kbd "C-o") 'open-line))
 
