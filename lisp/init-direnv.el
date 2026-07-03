a1971ed7663fdfa76be416f67371c285716df4fb
Arthur Lee <git@arthur.li>
2026-07-03 11:37:40 +0100

    direnv: enable remote

--- lisp/init-direnv.el
+++ lisp/init-direnv.el
@@ -3,7 +3,10 @@
 ;;; Code:
 
 (when (maybe-require-package 'envrc)
+  (setq envrc-remote 't)
   (with-eval-after-load 'envrc
+    (add-to-list 'envrc-supported-tramp-methods "sshx")
+    (add-to-list 'envrc-supported-tramp-methods "plinkx")
     (define-key envrc-mode-map (kbd "C-c e") 'envrc-command-map))
   (add-hook 'after-init-hook 'envrc-global-mode))
 
